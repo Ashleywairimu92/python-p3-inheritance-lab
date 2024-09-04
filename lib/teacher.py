390e1e279f6addf335e9ac44fdf66cda7e9662c9
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-09-04 22:51:01 +0300

    updates

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,11 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = ["Knowledge1", "Knowledge2"]  # Sample knowledge list
 
     def teach(self):
-        pass
+        # Returns a piece of knowledge from the list
+        return self.knowledge[0] if self.knowledge else None
